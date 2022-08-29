d4020f3710e5a26092d978db22c628711afe359e
Fazt <fazt@faztweb.com>
2022-08-29 03:50:57 -0500

    first commit

---
+++ tasks/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
+        }
+        
